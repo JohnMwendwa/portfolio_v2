f71940f32dc5c894f2f31f2e274fccd62aa2619e
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-06-14 20:39:22 +0300

    Create hobbies component

---
+++ src/components/sections/Hobbies.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Hobbies = () => {
+  return (
+    <div className="my-16 text-lg sm:text-xl max-w-5xl mx-auto">
+      <h2 className="font-bold text-yellow-400 text-5xl mb-16 text-center">
+        Hobbies
+      </h2>
+      <p>
+        Whenever I&apos;m not coding, I tend to engage myself in activties that
+        build me up. Some of these activities include but not limited to the
+        following :
+      </p>
+      <div className="py-3 space-y-4">
+        <p>
+          <span className="text-yellow-400 font-bold">Skating : </span>
+          To tell you the truth, I love skating. It&apos;s one of those things I
+          can hardly let a day pass by without even taking atleast a 5 minutes
+          ride around my neighborhood.
+        </p>
+        <p>
+          <span className="text-yellow-400 font-bold">Travelling : </span>
+          If you&apos;re not a kenyan, you wouldn&apos;t understand my obsession
+          with travelling, especially outside the city. As a nation, we&apos;ve
+          been endowed with great sites that are just breath-taking.
+        </p>
+        <p>
+          <span className="text-yellow-400 font-bold">Cooking : </span>
+          I&apos;m not a great fan of fast foods and I believe they should be
+          taken once in a while and so I have learned to cook my dishes and boy
+          ooh boy do I love their taste. Well, I know not everyone finds my
+          dishes as tasty as I do, but need I remind them the opinion of my
+          stomach and only my stomach concerning this matter supersedes all 😎.
+        </p>
+        <p>
+          <span className="text-yellow-400 font-bold">Movies : </span>
+          I&apos;m also a big fan of movies and series. As I was growing up, my
+          obsession with them always got me into trouble with my school
+          teachers, but that&apos;s a story for another day.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hobbies;
