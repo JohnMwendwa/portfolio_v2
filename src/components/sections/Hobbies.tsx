8b45a8d4a364c0772de5149e360c141b918422c1
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-06-15 16:08:22 +0300

    add animations

--- src/components/sections/Hobbies.tsx
+++ src/components/sections/Hobbies.tsx
@@ -1,4 +1,55 @@
-import React from "react";
+import { motion, Variants } from "framer-motion";
+
+interface HobbieProps {
+  title: string;
+  description: string;
+}
+
+const HobbieDetails: HobbieProps[] = [
+  {
+    title: "Skating",
+    description:
+      "To tell you the truth, I love skating. It's one of those things I can hardly let a day pass by without even taking atleast a 5 minutes ride around my neighborhood.",
+  },
+  {
+    title: "Travelling",
+    description:
+      "If you're not a kenyan, you wouldn't understand my obsession with travelling, especially outside the city. As a nation, we've been endowed with great sites that are just breath-taking.",
+  },
+  {
+    title: "Cooking",
+    description:
+      "I'm not a great fan of fast foods and I believe they should be taken once in a while and so I have learned to cook my dishes and boy ooh boy do I love their taste. Well, I know not everyone finds my dishes as tasty as I do, but need I remind them the opinion of my stomach and only my stomach concerning this matter supersedes all 😎.",
+  },
+  {
+    title: "Movies",
+    description:
+      "I'm also a big fan of movies and series. As I was growing up, my obsession with them always got me into trouble with my school teachers, but that's a story for another day.",
+  },
+];
+
+const Container: Variants = {
+  initial: {},
+  animate: {
+    transition: {
+      staggerChildren: 0.75,
+    },
+  },
+};
+
+const Word: Variants = {
+  initial: {
+    opacity: 0,
+    y: 50,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
 
 const Hobbies = () => {
   return (
@@ -11,34 +62,19 @@ const Hobbies = () => {
         build me up. Some of these activities include but not limited to the
         following :
       </p>
-      <div className="py-3 space-y-4">
-        <p>
-          <span className="text-yellow-400 font-bold">Skating : </span>
-          To tell you the truth, I love skating. It&apos;s one of those things I
-          can hardly let a day pass by without even taking atleast a 5 minutes
-          ride around my neighborhood.
-        </p>
-        <p>
-          <span className="text-yellow-400 font-bold">Travelling : </span>
-          If you&apos;re not a kenyan, you wouldn&apos;t understand my obsession
-          with travelling, especially outside the city. As a nation, we&apos;ve
-          been endowed with great sites that are just breath-taking.
-        </p>
-        <p>
-          <span className="text-yellow-400 font-bold">Cooking : </span>
-          I&apos;m not a great fan of fast foods and I believe they should be
-          taken once in a while and so I have learned to cook my dishes and boy
-          ooh boy do I love their taste. Well, I know not everyone finds my
-          dishes as tasty as I do, but need I remind them the opinion of my
-          stomach and only my stomach concerning this matter supersedes all 😎.
-        </p>
-        <p>
-          <span className="text-yellow-400 font-bold">Movies : </span>
-          I&apos;m also a big fan of movies and series. As I was growing up, my
-          obsession with them always got me into trouble with my school
-          teachers, but that&apos;s a story for another day.
-        </p>
-      </div>
+      <motion.div
+        className="py-3 space-y-4"
+        initial="initial"
+        whileInView="animate"
+        variants={Container}
+      >
+        {HobbieDetails.map((hobbie, idx) => (
+          <motion.p key={idx} variants={Word}>
+            <span className="text-yellow-400 font-bold">{hobbie.title} : </span>
+            {hobbie.description}
+          </motion.p>
+        ))}
+      </motion.div>
     </div>
   );
 };
