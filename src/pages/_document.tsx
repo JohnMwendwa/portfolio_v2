6823bf4c9617fe99e8159693e16293e0715b70af
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-06-15 18:49:51 +0300

    add bg color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="scroll-smooth">
+    <Html lang="en" className="scroll-smooth bg-gray-900">
       <Head />
       <body>
         <Main />
