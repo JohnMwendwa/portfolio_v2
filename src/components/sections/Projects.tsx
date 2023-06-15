45ed01628cbf7c89dcff31028940d89a5b8af0ca
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-06-15 12:28:07 +0300

    animate project cards

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -58,11 +58,15 @@ const ProectDetails = [
 const Projects = () => {
   return (
     <div className="my-8 px-6 sm:px-16 w-full">
-      <h1 className="font-bold text-white text-4xl sm:text-6xl mb-16 w-full text-center uppercase tracking-tight">
+      <h1 className="font-bold text-yellow-400 text-4xl sm:text-6xl mb-16 w-full text-center uppercase tracking-tight">
         Projects
       </h1>
+
       <div className="grid grid-cols-12 gap-y-8 md:gap-12 px-4">
-        <div
+        <motion.div
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1.5 }}
           className="col-span-12 w-full flex flex-col md:flex-row
          items-center justify-between rounded-3xl border border-black shadow-2xl p-4 sm:p-12 bg-white"
         >
@@ -77,7 +81,9 @@ const Projects = () => {
             />
           </div>
           <div className="md:w-1/2 flex flex-col justify-between items-start pl-6 mt-2 md:mt-0">
-            <span className="text-blue-400 font-medium text-xl">Featured</span>
+            <span className="text-yellow-700 font-medium text-xl">
+              Featured
+            </span>
             <h2 className="py-2 font-bold text-4xl">
               {ProectDetails[0].title}
             </h2>
@@ -87,7 +93,7 @@ const Projects = () => {
               {ProectDetails[0].technologies.map((tech, idx) => (
                 <span
                   key={idx}
-                  className="inline-flex flex-wrap px-3 py-1 mt-1 mr-2 rounded-lg bg-orange-200/50 text-sm saturate-200"
+                  className="inline-flex flex-wrap px-3 py-1 mt-1 mr-2 rounded-lg bg-yellow-400/20 text-sm"
                 >
                   {tech}
                 </span>
@@ -97,16 +103,19 @@ const Projects = () => {
               <Link
                 href={ProectDetails[0].demo}
                 target="_blank"
-                className="bg-orange-700 text-white px-6 py-2 text-sm rounded-lg"
+                className="bg-orange-700 hover:bg-orange-600 text-white px-6 py-2 text-sm rounded-lg"
               >
                 Live Demo
               </Link>
             </div>
           </div>
-        </div>
+        </motion.div>
 
         {ProectDetails.slice(1, 4).map((project, idx) => (
-          <div
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.7 * idx }}
             key={idx}
             className="col-span-12 md:col-span-6 xl:col-span-4 flex flex-col border border-gray-200 rounded-2xl shadow-sm shadow-gray-100"
           >
@@ -124,7 +133,7 @@ const Projects = () => {
               />
             </motion.div>
 
-            <div className="mt-3 px-4 py-2 text-gray-100 flex-1">
+            <div className="mt-3 px-4 py-2 text-yellow-100 flex-1">
               <h3 className="mb-2 text-2xl font-bold tracking-tight">
                 {project.title}
               </h3>
@@ -137,18 +146,18 @@ const Projects = () => {
                 {project.technologies.map((tech, idx) => (
                   <span
                     key={idx}
-                    className="inline-flex flex-wrap px-3 py-1 mt-1 mr-2 rounded-lg bg-orange-400/20 text-sm saturate-200"
+                    className="inline-flex flex-wrap px-3 py-1 mt-1 mr-2 rounded-lg bg-yellow-400/20 text-sm"
                   >
                     {tech}
                   </span>
                 ))}
               </p>
             </div>
-            <div className="flex justify-between items-center mb-3 px-4 space-x-2">
+            <div className="flex justify-start items-center mb-3 px-4 space-x-2">
               <Link
                 href={project.demo}
                 target="_blank"
-                className="bg-orange-700 text-white px-6 py-2 text-sm rounded-lg"
+                className="bg-yellow-700 hover:bg-yellow-600 text-white px-6 py-2 text-sm rounded-lg"
               >
                 Live Demo
               </Link>
@@ -156,13 +165,13 @@ const Projects = () => {
                 <Link
                   href={project.source}
                   target="_blank"
-                  className="bg-blue-800 text-white px-6 py-2 text-sm rounded-lg"
+                  className="bg-orange-700 hover:bg-orange-600 text-white px-6 py-2 text-sm rounded-lg"
                 >
                   Source Code
                 </Link>
               )}
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
