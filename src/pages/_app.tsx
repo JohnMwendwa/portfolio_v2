fc6611ca8882384fda1e5d25e107e727db47ecd9
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-06-16 22:18:57 +0300

    add site metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
 import { motion, AnimatePresence, Variants } from "framer-motion";
@@ -14,6 +15,60 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <>
+      <Head>
+        <title>John Mwendwa - Full Stack web developer</title>
+
+        <meta
+          name="description"
+          content="John Mwendwa is a passionate full stack web developer with an extensive knowledge in HTML, CSS, Javascript, React, Next.js, Express, Node.js, MySQL and MongoDB."
+          key="desc"
+        />
+
+        <meta
+          name="keywords"
+          content="Full Stack web developer, John Mwendwa, Mwendwa, Front-End, React, JavaScript, Node.js, Next.js, MongoDB , SEO"
+        />
+
+        <meta name="author" content="John Mwendwa" />
+        <meta
+          property="og:title"
+          content="John Mwendwa | Full Stack web developer"
+        />
+
+        <meta
+          property="og:description"
+          content="John Mwendwa is a passionate full stack web developer with an extensive knowledge in HTML, CSS, Javascript, React, Next.js, Express, Node.js, MySQL and MongoDB."
+        />
+
+        <meta name="og:type" content="website" />
+
+        <meta property="og:url" content="https://johnmwendwa.me" />
+
+        <meta property="og:site_name" content="John Mwendwa" />
+
+        <meta property="og:locale" content="en_KE" />
+
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon.png" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <AnimatePresence>
         <motion.div
           className={`${inter.variable} font-sans bg-gray-900`}
