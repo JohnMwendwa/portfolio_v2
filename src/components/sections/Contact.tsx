56d85e279e3c7b59e25986de2aec506e6642477a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-06-17 23:56:01 +0300

    wrap the page with te section container

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -5,6 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 import WhatsappIcon from "assets/whatsapp";
 import MailIcon from "assets/mail";
+import SectionContainer from "components/SectionContainer";
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -110,111 +111,118 @@ const Contact = () => {
     }
   };
   return (
-    <div className="relative flex flex-col items-center justify-center h-full px-8">
-      <ToastContainer />
-      <h1 className="font-bold text-yellow-400 text-4xl sm:text-5xl mb-2 w-full text-center uppercase tracking-tight">
-        Contact Me
-      </h1>
+    <div className="relative flex flex-col items-center justify-center h-full my-4 sm:my-8">
+      <SectionContainer>
+        <ToastContainer />
+        <div className="flex flex-col items-center justify-center w-full px-4">
+          <h1 className="font-bold text-yellow-400 text-4xl sm:text-5xl mb-2 w-full text-center uppercase tracking-tight">
+            Contact Me
+          </h1>
 
-      <div className="flex justify-center space-x-4 text-white text-sm mb-6">
-        <Link
-          href={"https://wa.me/+254716237927"}
-          target="_blank"
-          className="group hover:text-yellow-400 inline-flex flex-col items-center justify-center"
-        >
-          Whatsapp
-          <WhatsappIcon className="group-hover:fill-yellow-400 mt-1" />
-        </Link>
-        <Link
-          href={"mailto:hey@ohnmwendwa.me"}
-          className="group hover:text-yellow-400 inline-flex flex-col items-center justify-center"
-        >
-          Email
-          <MailIcon className="group-hover:fill-yellow-400 mt-1" />
-        </Link>
-      </div>
+          <div className="flex justify-center space-x-4 text-white text-sm mb-6">
+            <Link
+              href={"https://wa.me/+254716237927"}
+              target="_blank"
+              className="group hover:text-yellow-400 inline-flex flex-col items-center justify-center"
+            >
+              Whatsapp
+              <WhatsappIcon className="group-hover:fill-yellow-400 mt-1" />
+            </Link>
+            <Link
+              href={"mailto:hey@ohnmwendwa.me"}
+              className="group hover:text-yellow-400 inline-flex flex-col items-center justify-center"
+            >
+              Email
+              <MailIcon className="group-hover:fill-yellow-400 mt-1" />
+            </Link>
+          </div>
 
-      <form className="w-full max-w-lg appearance-none" onSubmit={handleSubmit}>
-        <div className="relative w-full">
-          <input
-            id="name"
-            type="text"
-            placeholder="Enter your name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            className="peer rounded-sm ring-2 ring-yellow-600 focus:ring focus:ring-yellow-400 bg-transparent w-full h-10 px-4 focus:outline-none placeholder-transparent text-white"
-          />
-          <label
-            htmlFor="name"
-            className="absolute transition-all left-0 -top-3.5 ml-4 px-2 bg-gray-900 peer-placeholder-shown:top-2 peer-focus:-top-3.5 text-gray-300"
+          <form
+            className="w-full max-w-lg appearance-none"
+            onSubmit={handleSubmit}
           >
-            Name
-          </label>
+            <div className="relative w-full">
+              <input
+                id="name"
+                type="text"
+                placeholder="Enter your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                className="peer rounded-sm ring-2 ring-yellow-600 focus:ring focus:ring-yellow-400 bg-transparent w-full h-10 px-4 focus:outline-none placeholder-transparent text-white"
+              />
+              <label
+                htmlFor="name"
+                className="absolute transition-all left-0 -top-3.5 ml-4 px-2 bg-gray-900 peer-placeholder-shown:top-2 peer-focus:-top-3.5 text-gray-300"
+              >
+                Name
+              </label>
+            </div>
+            <div className="relative w-full mt-6">
+              <input
+                id="email"
+                type="emal"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="peer rounded-sm ring-2  ring-yellow-600 focus:ring focus:ring-yellow-400 bg-transparent w-full h-10 px-4 focus:outline-none placeholder-transparent text-white"
+              />
+              <label
+                htmlFor="email"
+                className="absolute transition-all left-0 -top-3.5 ml-4 px-2 bg-gray-900 peer-placeholder-shown:top-2 peer-focus:-top-3.5 text-gray-300"
+              >
+                Email
+              </label>
+            </div>
+            <div className="relative w-full mt-6">
+              <input
+                id="subject"
+                type="text"
+                placeholder="Enter subject"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+                required
+                className="peer rounded-sm ring-2  ring-yellow-600 focus:ring focus:ring-yellow-400 bg-transparent w-full h-10 px-4 focus:outline-none placeholder-transparent text-white"
+              />
+              <label
+                htmlFor="subject"
+                className="absolute transition-all left-0 -top-3.5 ml-4 px-2 bg-gray-900 peer-placeholder-shown:top-2 peer-focus:-top-3.5 text-gray-300"
+              >
+                Subject
+              </label>
+            </div>
+            <div className="relative w-full mt-6">
+              <textarea
+                id="textarea"
+                placeholder="Enter your message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+                rows={3}
+                className="peer rounded-sm ring-2 ring-yellow-600 focus:ring focus:ring-yellow-400 bg-transparent w-full px-4 py-2 focus:outline-none placeholder-transparent text-white"
+              />
+              <label
+                htmlFor="textarea"
+                className="absolute transition-all left-0 -top-3.5 ml-4 px-2 bg-gray-900 text-gray-300"
+              >
+                Message
+              </label>
+            </div>
+            <div className="flex justify-center w-full mt-2">
+              <button
+                type="submit"
+                className={`${
+                  loading ? "bg-gray-400" : "bg-orange-700 hover:bg-orange-600"
+                } text-white px-6 py-3 rounded-md hover:scale-105 focus:ring-yellow-400 focus:scale-105 focus:ring focus:ring-offset-2 ease-linear transition-all`}
+                disabled={loading}
+              >
+                Send Feedback
+              </button>
+            </div>
+          </form>
         </div>
-        <div className="relative w-full mt-6">
-          <input
-            id="email"
-            type="emal"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="peer rounded-sm ring-2  ring-yellow-600 focus:ring focus:ring-yellow-400 bg-transparent w-full h-10 px-4 focus:outline-none placeholder-transparent text-white"
-          />
-          <label
-            htmlFor="email"
-            className="absolute transition-all left-0 -top-3.5 ml-4 px-2 bg-gray-900 peer-placeholder-shown:top-2 peer-focus:-top-3.5 text-gray-300"
-          >
-            Email
-          </label>
-        </div>
-        <div className="relative w-full mt-6">
-          <input
-            id="subject"
-            type="text"
-            placeholder="Enter subject"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-            required
-            className="peer rounded-sm ring-2  ring-yellow-600 focus:ring focus:ring-yellow-400 bg-transparent w-full h-10 px-4 focus:outline-none placeholder-transparent text-white"
-          />
-          <label
-            htmlFor="subject"
-            className="absolute transition-all left-0 -top-3.5 ml-4 px-2 bg-gray-900 peer-placeholder-shown:top-2 peer-focus:-top-3.5 text-gray-300"
-          >
-            Subject
-          </label>
-        </div>
-        <div className="relative w-full mt-6">
-          <textarea
-            id="textarea"
-            placeholder="Enter your message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            required
-            rows={3}
-            className="peer rounded-sm ring-2 ring-yellow-600 focus:ring focus:ring-yellow-400 bg-transparent w-full px-4 py-2 focus:outline-none placeholder-transparent text-white"
-          />
-          <label
-            htmlFor="textarea"
-            className="absolute transition-all left-0 -top-3.5 ml-4 px-2 bg-gray-900 text-gray-300"
-          >
-            Message
-          </label>
-        </div>
-        <div className="flex justify-center w-full mt-2">
-          <button
-            type="submit"
-            className={`${
-              loading ? "bg-gray-400" : "bg-orange-700 hover:bg-orange-600"
-            } text-white px-6 py-3 rounded-md hover:scale-105 focus:ring-yellow-400 focus:scale-105 focus:ring focus:ring-offset-2 ease-linear transition-all`}
-            disabled={loading}
-          >
-            Send Feedback
-          </button>
-        </div>
-      </form>
+      </SectionContainer>
     </div>
   );
 };
--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import SectionContainer from "components/SectionContainer";
 
 interface ProectDetailsProps {
   image: {
@@ -86,91 +87,41 @@ const FramerImage = motion(Image);
 
 const Projects = () => {
   return (
-    <div className="my-8 px-6 sm:px-16 w-full">
-      <h1 className="font-bold text-yellow-400 text-4xl sm:text-5xl mb-4 sm:mb-16 w-full text-center uppercase tracking-tight">
-        Projects
-      </h1>
+    <div className="my-8">
+      <SectionContainer>
+        <h1 className="font-bold text-yellow-400 text-4xl sm:text-5xl mb-4 sm:mb-16 w-full text-center uppercase tracking-tight">
+          Projects
+        </h1>
 
-      <div className="grid grid-cols-12 gap-y-8 md:gap-12 px-4">
-        <motion.div
-          initial={{ opacity: 0, x: -50 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1.5 }}
-          className="col-span-12 w-full flex flex-col md:flex-row
-         items-center justify-between rounded-3xl border border-black shadow-2xl p-4 sm:p-12 bg-white"
-        >
-          <div className="flex flex-col w-full md:w-1/2 max-h-[300px] overflow-hidden rounded-lg border-2">
-            <Image
-              src={ProectDetails[0].image.src}
-              alt=""
-              width={300}
-              height={200}
-              className="w-full h-auto object-cover"
-              priority
-            />
-          </div>
-          <div className="md:w-1/2 flex flex-col justify-between items-start pl-6 mt-2 md:mt-0">
-            <span className="text-yellow-700 font-medium text-xl">
-              Featured
-            </span>
-            <h2 className="py-2 font-bold text-4xl">
-              {ProectDetails[0].title}
-            </h2>
-            <p>{ProectDetails[0].description}</p>
-            <p className="mt-2">
-              <span className="font-bold">Technologies : </span>
-              {ProectDetails[0].technologies.map((tech, idx) => (
-                <span
-                  key={idx}
-                  className="inline-flex flex-wrap px-3 py-1 mt-1 mr-2 rounded-lg bg-yellow-400/20 text-sm"
-                >
-                  {tech}
-                </span>
-              ))}
-            </p>
-            <div className="flex w-full justify-center items-center my-3">
-              <Link
-                href={ProectDetails[0].demo}
-                target="_blank"
-                className="bg-orange-700 hover:bg-orange-600 text-white px-6 py-2 text-sm rounded-lg"
-              >
-                Live Demo
-              </Link>
-            </div>
-          </div>
-        </motion.div>
-
-        {ProectDetails.slice(1, 4).map((project, idx) => (
+        <div className="grid grid-cols-12 gap-y-8 md:gap-12 px-4 sm:px-16">
           <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.4 * idx }}
-            key={idx}
-            className="col-span-12 md:col-span-6 xl:col-span-4 flex flex-col border border-gray-200 rounded-2xl shadow-sm shadow-gray-100"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1.5 }}
+            className="col-span-12 w-full flex flex-col md:flex-row
+         items-center justify-between rounded-3xl border border-black shadow-2xl p-4 sm:p-12 bg-white"
           >
-            <motion.div
-              className="w-full max-h-[300px] md:h-auto overflow-hidden rounded-t-2xl "
-              whileHover={{ scale: 0.95 }}
-              transition={{ duration: 0.2 }}
-            >
-              <FramerImage
-                src={project.image.src}
-                alt={project.image.alt}
-                className="min-w-full h-auto rounded-t-lg object-cover"
+            <div className="flex flex-col w-full md:w-1/2 max-h-[300px] overflow-hidden rounded-lg border-2">
+              <Image
+                src={ProectDetails[0].image.src}
+                alt=""
                 width={300}
-                height={300}
+                height={200}
+                className="w-full h-auto object-cover"
+                priority
               />
-            </motion.div>
-
-            <div className="mt-3 px-4 py-2 text-yellow-100 flex-1">
-              <h3 className="mb-2 text-2xl font-bold tracking-tight">
-                {project.title}
-              </h3>
-              <p className="mb-3 text-gray-300">{project.description}</p>
-
-              <p className="mb-3">
+            </div>
+            <div className="md:w-1/2 flex flex-col justify-between items-start pl-6 mt-2 md:mt-0">
+              <span className="text-yellow-700 font-medium text-xl">
+                Featured
+              </span>
+              <h2 className="py-2 font-bold text-4xl">
+                {ProectDetails[0].title}
+              </h2>
+              <p>{ProectDetails[0].description}</p>
+              <p className="mt-2">
                 <span className="font-bold">Technologies : </span>
-                {project.technologies.map((tech, idx) => (
+                {ProectDetails[0].technologies.map((tech, idx) => (
                   <span
                     key={idx}
                     className="inline-flex flex-wrap px-3 py-1 mt-1 mr-2 rounded-lg bg-yellow-400/20 text-sm"
@@ -179,28 +130,80 @@ const Projects = () => {
                   </span>
                 ))}
               </p>
-            </div>
-            <div className="flex justify-start items-center mb-3 px-4 space-x-2">
-              <Link
-                href={project.demo}
-                target="_blank"
-                className="bg-yellow-700 hover:bg-yellow-600 text-white px-4 sm:px-6 py-2 text-sm rounded-lg"
-              >
-                Live Demo
-              </Link>
-              {project.source && (
+              <div className="flex w-full justify-center items-center my-3">
                 <Link
-                  href={project.source}
+                  href={ProectDetails[0].demo}
                   target="_blank"
-                  className="bg-orange-700 hover:bg-orange-600 text-white p-4 sm:px-6 py-2 text-sm rounded-lg"
+                  className="bg-orange-700 hover:bg-orange-600 text-white px-6 py-2 text-sm rounded-lg"
                 >
-                  Source Code
+                  Live Demo
                 </Link>
-              )}
+              </div>
             </div>
           </motion.div>
-        ))}
-      </div>
+
+          {ProectDetails.slice(1, 4).map((project, idx) => (
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.4 * idx }}
+              key={idx}
+              className="col-span-12 md:col-span-6 xl:col-span-4 flex flex-col border border-gray-200 rounded-2xl shadow-sm shadow-gray-100"
+            >
+              <motion.div
+                className="w-full max-h-[300px] md:h-auto overflow-hidden rounded-t-2xl "
+                whileHover={{ scale: 0.95 }}
+                transition={{ duration: 0.2 }}
+              >
+                <FramerImage
+                  src={project.image.src}
+                  alt={project.image.alt}
+                  className="min-w-full h-auto rounded-t-lg object-cover"
+                  width={300}
+                  height={300}
+                />
+              </motion.div>
+
+              <div className="mt-3 px-4 py-2 text-yellow-100 flex-1">
+                <h3 className="mb-2 text-2xl font-bold tracking-tight">
+                  {project.title}
+                </h3>
+                <p className="mb-3 text-gray-300">{project.description}</p>
+
+                <p className="mb-3">
+                  <span className="font-bold">Technologies : </span>
+                  {project.technologies.map((tech, idx) => (
+                    <span
+                      key={idx}
+                      className="inline-flex flex-wrap px-3 py-1 mt-1 mr-2 rounded-lg bg-yellow-400/20 text-sm"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </p>
+              </div>
+              <div className="flex justify-start items-center mb-3 px-4 space-x-2">
+                <Link
+                  href={project.demo}
+                  target="_blank"
+                  className="bg-yellow-700 hover:bg-yellow-600 text-white px-4 sm:px-6 py-2 text-sm rounded-lg"
+                >
+                  Live Demo
+                </Link>
+                {project.source && (
+                  <Link
+                    href={project.source}
+                    target="_blank"
+                    className="bg-orange-700 hover:bg-orange-600 text-white p-4 sm:px-6 py-2 text-sm rounded-lg"
+                  >
+                    Source Code
+                  </Link>
+                )}
+              </div>
+            </motion.div>
+          ))}
+        </div>
+      </SectionContainer>
     </div>
   );
 };
