6fd8047daede50f8fa0637400258cd95a985ad11
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-06-18 00:06:14 +0300

    set container to take full width

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -2,7 +2,9 @@ import { ReactNode } from "react";
 
 const SectionContainer = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6  xl:px-0">{children}</div>
+    <div className="mx-auto max-w-7xl px-4 sm:px-6  xl:px-0 w-full">
+      {children}
+    </div>
   );
 };
 
