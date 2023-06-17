990032279accec85ad929b892e8077131f4363cf
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-06-17 22:04:13 +0300

    add exit animations

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -8,9 +8,10 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <motion.div
       className="grid grid-rows-[auto_1fr_auto] min-h-screen dynamic relative"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.75, delay: 0.75 }}
+      exit={{
+        opacity: 0,
+      }}
+      transition={{ duration: 0.75 }}
     >
       <Navbar />
       <main>{children}</main>
